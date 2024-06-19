9b04d91237da92c4a61acfda8acd392c1763e852
Jitender Kumar <jkofficial1712@gmail.com>
2024-06-19 07:27:48 +0530

    lambda fails

--- lambdas/hello-lambda.ts
+++ lambdas/hello-lambda.ts
@@ -1,11 +1,45 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { Logger } from '@aws-lambda-powertools/logger';
+
+const logger = new Logger({
+    serviceName: 'hello-lambda-handler',
+});
+
+// Handler fails as it increases the default timeout of 3 seconds
+
+// export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+//     await new Promise(resolve => setTimeout(resolve, 6000)); // Simulate a delay of 6 seconds
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify(event),
+//     };
+// };
+
+// Handler fails as it raises an exception
+
 
 export const handler = async (event: APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> => {
-    console.log(event);
+    logger.info('Got event', JSON.stringify(event));
+    try {
+        if (event)
+        {
+            throw new  Error("Event Error");
+        }
+        return {
+            statusCode: 200,
+            // let's just return the whole event
+            body: JSON.stringify(event),
+        };
+    }
+    catch (error) {
+        logger.error(`Error : ${JSON.stringify(error)}`);
+        throw new  Error("re raising the exception");
+    }
 
-    return {
-        statusCode: 200,
-        // let's just return the whole event
-        body: JSON.stringify(event),
-    };
+
+
 };
+
+
+// Handler fails as it run out of memory
+/* TO DO */
